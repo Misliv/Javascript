aee948e47dcba55ff0d4ec0c7a31e68a3499636f
May <mayandmays@hotmail.com>
2021-02-04 12:25:21 +0100

    2.1 af

---
+++ src/H2/Opdracht2.js
@@ -0,0 +1,5 @@
+
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Pan_troglodytes_%28male%29.jpg/280px-Pan_troglodytes_%28male%29.jpg";
+document.getElementById("aap2").src = "https://resource.wur.nl/upload_mm/b/0/2/06e75304-7465-4a08-bffa-2efce553d4ea_shutterstock_508812640_3b56aa0e_570x456.jpg";
