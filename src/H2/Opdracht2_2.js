2023ec202f42fb31fd016a9fc841083c4e6f1ac2
May <mayandmays@hotmail.com>
2021-02-04 13:28:15 +0100

    H2 done

--- src/H2/Opdracht2_2.js
+++ src/H2/Opdracht2_2.js
@@ -1,7 +1,9 @@
 document.getElementById("bl1").style.backgroundColor = "cyan";
 document.getElementById("bl1").style.textAlign = "center";
 document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "20px";
 document.getElementById("bl2").style.textAlign = "center";
 document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "50px";
 document.getElementById("bl3").style.textAlign = "right";
 document.getElementById("bl4").style.backgroundColor = "red";
