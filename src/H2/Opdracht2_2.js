bc473f2a306e1c1706ce34f2ab47419136273336
May <mayandmays@hotmail.com>
2021-02-04 13:00:04 +0100

    H2 almost done

---
+++ src/H2/Opdracht2_2.js
@@ -0,0 +1,7 @@
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.textAlign = "right";
+document.getElementById("bl4").style.backgroundColor = "red";
